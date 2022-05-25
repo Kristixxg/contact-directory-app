6fef30df81187fdeeb9b746587957634186a5e2b
Maung Htike <maunghtike1999@gmail.com>
2022-05-25 12:16:35 -0700

    add database features

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,23 +3,52 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+    openDB("contacts", 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains("contacts")) {
+                console.log("Contacts DB already exists");
+                return;
+            }
+            db.createObjectStore("contacts", { keyPath: "id", autoIncrement: true });
+            console.log("Contacts DB created");
+        },
+    });
 };
 
 
 // TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
+export const postDb = async (name, home, cell, email) => {
+    console.log("Posting to DB...");
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+    const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    console.log('Getting from DB...');
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readonly');
+    const store = tx.objectStore('contacts');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    console.log('DELETE from the database', id);
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+    const request = store.delete(id);
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 initdb();
